27e773bed07959a7cb79109c5a32620c0fba94a4
jrsoft2000 <jrlsoft2000@gmail.com>
2020-07-11 10:13:05 -0400

    first php prog

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> GiTHUB phpApps site</title
+</head>
+<body>
+<h1>Wellcome to my phpApps</h1>
+<?php
+echo 'php apps with javascript / css / html ';
+?>
+</body>
+</html>
+
